6eeb69e36e80f41f0ec535c68b3a9a55cad68c5c
Othman Nouaoury <noouaoury_othman@emsi-edu.ma>
2022-02-09 19:14:15 +0100

    changement du premier projet

--- src/pages/Projet1.jsx
+++ src/pages/Projet1.jsx
@@ -2,24 +2,23 @@ import ContentToggler from '../components/ContentToggler';
 import styles from '../components/Gallerie.module.css';
 import projet1 from '../resources/projet1.zip';
 import capture1 from '../resources/projet1_capture1.png';
-import capture2 from '../resources/projet1_capture2.png';
-import nodejs from '../resources/nodejs.png';
+import spring from '../resources/spring_boot.png';
 
 export default function Projet1() {
     return <div>
         <h2>Projet : Plateforme de réservation des tickets de cinéma</h2>
         <a href={projet1} className="btn center" download="projet1.zip">Télécharger le projet!</a>
         <ContentToggler title="Description du projet" >
-            <p>Ce projet représente une plateforme de vente des jeux vidéos avec une description de chaque jeu.</p>
+            <p>Ce projet représente une application web pour faire une analyse des sentiments sur des pages Faccebook 
+                à partir de leurs commentaires.</p>
             <p>
-                Le projet a été réalisé avec :  NodeJS <img src={nodejs} width="40" height="30"/>
+                Le projet a été réalisé avec :  Spring boot <img src={spring} width="40" height="30"/>
             </p>
         </ContentToggler>
 
 
         <div className={styles.gallerie}>
             <img src={capture1} alt="capture 1" key="1" />
-            <img src={capture2} alt="capture 2" key="2" />
         </div>
     </div>
 }
